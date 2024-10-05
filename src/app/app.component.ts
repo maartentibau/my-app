ef9311a8b801dd105277688205d8f98dafd349fa
Maarten Tibau <maarten@webtrix.be>
2024-10-05 18:40:25 +0200

    add directive to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TypedDirective } from 'ng-directives';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TypedDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'my-app';
+
+  value = {
+    a: 'foobar',
+    b: {
+      c: 10
+    }
+  }
 }
